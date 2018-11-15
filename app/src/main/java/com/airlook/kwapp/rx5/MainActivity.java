099eec8e22a48a242cee593f6bcaadf88fc879a6
lili <shandongqdlpl@163.com>
2018-11-15 19:34:54 +0800

    four

--- app/src/main/java/com/airlook/kwapp/rx5/MainActivity.java
+++ app/src/main/java/com/airlook/kwapp/rx5/MainActivity.java
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
             case R.id.btn_flatmap:
+
+
                 flatMap();
                 break;
         }
